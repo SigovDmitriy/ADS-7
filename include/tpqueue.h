f1d8a4c95c3a0dc263c36b4f0c0138c1e2acaa32
Сигов Дмитрий <99177871+SigovDmitriy@users.noreply.github.com>
2022-04-28 19:26:31 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,66 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct ITEM {
+    T content;
+    ITEM * next;
+    ITEM * prev;
+  };
+  TPQueue :: ITEM * create(const T&);
+  ITEM* head;
+  ITEM* tail;
+ public:
+  TPQueue():head(nullptr), tail(nullptr) {}
+  void push(const T&);
+  T pop();
 };
 
 struct SYM {
   char ch;
   int prior;
 };
+template<typename T>
+typename TPQueue<T>::ITEM * TPQueue<T>::create(const T& content) {
+  ITEM * item = new ITEM;
+  item -> content = content;
+  item -> next = nullptr;
+  item -> prev = nullptr;
+  return item;
+}
+template<typename T>
+void TPQueue <T>::push(const T& content) {
+  ITEM * tmp = head;
+  ITEM * item = create(content);
+  while (tmp && tmp -> content.prior >= content.prior)
+    tmp = tmp -> next;
+  if (!tmp && head) {
+    tail -> next = item;
+    tail -> next -> prev = tail;
+    tail = item;
+  } else if (!tmp && !head) {
+    head=tail = item;
+  } else if (!tmp -> prev) {
+    tmp -> prev = item;
+    item -> next = tmp;
+    head = item;
+  } else {
+    tmp -> prev -> next = item;
+    item -> prev = tmp -> prev;
+    item -> next = tmp;
+    tmp -> prev = item;
+  }
+}
+template<typename T>
+T TPQueue<T>::pop() {
+  ITEM * tmp = head -> next;
+  if (tmp) {
+    tmp -> prev = nullptr;
+  }
+  T content = head -> content;
+  delete head;
+  head = tmp;
+  return content;
+}
 
 #endif  // INCLUDE_TPQUEUE_H_
